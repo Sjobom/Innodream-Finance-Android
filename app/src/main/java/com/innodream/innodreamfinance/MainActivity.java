2bf2f3c2695bc681368f3c1b4f524cd1cf2b3d6b
Anders Sjöbom <anders.sjobom@live.se>
2018-04-14 12:04:25 +0200

    feat: added recyclerview for ticker list

--- app/src/main/java/com/innodream/innodreamfinance/MainActivity.java
+++ app/src/main/java/com/innodream/innodreamfinance/MainActivity.java
@@ -2,54 +2,65 @@ package com.innodream.innodreamfinance;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-import com.innodream.innodreamfinance.finance.stock;
-import com.innodream.innodreamfinance.http.HttpGetRequest;
 import com.innodream.innodreamfinance.http.OnHttpReadyCallback;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Iterator;
+import com.innodream.innodreamfinance.model.Ticker;
 
 public class MainActivity extends Activity implements OnHttpReadyCallback {
 
-    TextView resultTextView;
-    EditText companyNameEditText;
-    Button getDataButton;
     private MainActivity mainActivity = this;
+    private RecyclerView ticker_recycler_view;
+    private RecyclerView.Adapter ticker_adapter;
+    private RecyclerView.LayoutManager ticker_layout_manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultTextView = findViewById(R.id.resultTextView);
-        companyNameEditText = findViewById(R.id.CompanyNameEditText);
-        getDataButton = findViewById(R.id.getDataButton);
+        inflate_company_recycler_view();
     }
 
-    public void getDataPressed(View v) {
-        String company_ticker = companyNameEditText.getText().toString();
-        stock.get_latest_stock_price(mainActivity, company_ticker);
+    private void inflate_company_recycler_view() {
+        ticker_recycler_view = findViewById(R.id.ticker_recycler_view);
+        ticker_recycler_view.setHasFixedSize(true); // Only for performance improvements
+
+        ticker_layout_manager = new LinearLayoutManager(this);
+        ticker_recycler_view.setLayoutManager(ticker_layout_manager);
+
+        // Add lines between the rows
+        DividerItemDecoration ticker_divider_decoration = new DividerItemDecoration(
+                ticker_recycler_view.getContext(),
+                DividerItemDecoration.VERTICAL
+        );
+        ticker_recycler_view.addItemDecoration(ticker_divider_decoration);
+
+        // Get the tickers
+        Ticker[] tickers = new Ticker[3];
+        tickers[0] = new Ticker("ABB:ST", "ABB");
+        tickers[1] = new Ticker("ERIC-B.ST", "Ericsson B");
+        tickers[2] = new Ticker("ALFA.ST", "Alfa Laval");
+
+        ticker_adapter = new TickerAdapter(tickers);
+
+        ticker_recycler_view.setAdapter(ticker_adapter);
+
     }
 
 
     @Override
     public void onHttpReady(String result) {
-        try {
-            JSONObject jsonObject = new JSONObject(result);
-            JSONObject time_series = jsonObject.getJSONObject("Time Series (Daily)");
-            Iterator<String> keys = time_series.keys();
-            String price = time_series.getJSONObject(keys.next()).getString("4. close");
-            resultTextView.setText(price);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            JSONObject jsonObject = new JSONObject(result);
+//            JSONObject time_series = jsonObject.getJSONObject("Time Series (Daily)");
+//            Iterator<String> keys = time_series.keys();
+//            String price = time_series.getJSONObject(keys.next()).getString("4. close");
+//            resultTextView.setText(price);
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
     }
 }
